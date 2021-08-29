03d7a03a941f0cdff7d01394285ca1a1d6712a17
Miguel_Reis <guelsouzar@gmail.com>
2021-08-29 20:42:28 -0300

    fazendo atividades atuais

--- turma_15/exercicios/modulo-1-fundamentos/Bloco-07:-Introdução-à-JavaScript-ES6-e-Testes-Unitários/Dia01/Atividade/Parte 2/script4.js
+++ turma_15/exercicios/modulo-1-fundamentos/Bloco-07:-Introdução-à-JavaScript-ES6-e-Testes-Unitários/Dia01/Atividade/Parte 2/script4.js
@@ -1,2 +1,6 @@
-let nome = 'rodrigo'
-console.log(`Trybe${nome}aqui!`)
+let nome = 'Miguel'
+console.log(`Trybe ${nome} aqui!`)
+
+let array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+console.log(`Minhas cinco principais habilidades são: ${array} #goTrybe`)
